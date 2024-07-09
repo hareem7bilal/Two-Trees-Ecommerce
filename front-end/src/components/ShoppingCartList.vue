23321e2c9b990358764dffb71a4bc0b0072e8c8e
Hareem Bilal <91612899+hareem7bilal@users.noreply.github.com>
2024-07-09 14:54:25 +0500

    remove node_modules

---
+++ front-end/src/components/ShoppingCartList.vue
@@ -0,0 +1,18 @@
+<template>
+    <div v-for="product in cartItems" :key="product.id" class="product-container">
+        <img :src="product.imageName" class="product-image" />
+        <div class="details-wrap">
+            <h3 class="product-name">{{ product.name }}</h3>
+            <p class="product-price">{{ product.price }}</p>
+        </div>
+        <button class="remove-button">Remove from Cart</button>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: "ShoppingCartList",
+    props: ['cartItems'],
+}
+</script>
---
+++ front-end/src/pages/ShoppingCartPage.vue
@@ -0,0 +1,27 @@
+<template>
+    <h1>Shopping Cart Page</h1>
+    <div v-if="cartItems.length > 0">
+        <ShoppingCartList :cartItems="cartItems" />
+        <button class="checkout-button">Proceed to Checkout</button>
+    </div>
+    <div v-if="cartItems.length === 0">
+        You currently have no items in your cart!
+    </div>
+
+</template>
+
+<script>
+import ShoppingCartList from '@/components/ShoppingCartList.vue';
+import { cartItems } from '@/temp-data';
+export default {
+    name: 'ShoppingCartPage',
+    components: {
+        ShoppingCartList,
+    },
+    data() {
+        return {
+            cartItems,
+        }
+    }
+}
+</script>
